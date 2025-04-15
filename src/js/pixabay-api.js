0ecd101dfb01db91e2e0929a9cc592edfe54fbd8
Alexandr Krivsic <o.kryvshych@gmail.com>
2025-04-16 00:56:35 +0200

    hw11

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = '49742817-2a5c8f84523d00bf5c0bae38f';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export async function getImagesByQuery(query) {
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  };
+
+  const response = await axios.get(BASE_URL, { params });
+  return response.data;
+}
